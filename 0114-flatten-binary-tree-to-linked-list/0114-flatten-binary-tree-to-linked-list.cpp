f5f67a0c3eac96e6b6a457d88270f0558a37905d
Tharun <tharuntej7373@gmail.com>
2026-08-27 22:36:08 +0530

    Time: 2 ms (4.31%), Space: 17.5 MB (52.59%) - LeetHub

---
+++ 0114-flatten-binary-tree-to-linked-list/0114-flatten-binary-tree-to-linked-list.cpp
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* flattenTree(TreeNode* node) {
+        if (!node) return nullptr;
+
+        TreeNode* leftSubTree = flattenTree(node -> left);
+        TreeNode* rightSubTree = flattenTree(node -> right);
+
+        if (node -> left) {
+            TreeNode* temp = node -> right;
+            node -> right = node -> left;
+            node -> left = nullptr;
+
+            leftSubTree -> right = temp;
+        }
+
+        if (rightSubTree) return rightSubTree;
+        if (leftSubTree) return leftSubTree;
+
+        return node;
+    }
+    void flatten(TreeNode* root) {
+        flattenTree(root);
+    }
+};
